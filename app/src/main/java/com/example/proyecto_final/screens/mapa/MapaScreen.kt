74eacdd7cce10f6537b8c1df80620c7dee3be4ec
JRVelarde <96296424+JRVelarde@users.noreply.github.com>
2024-02-12 19:40:05 +0100

    mapasv2

--- app/src/main/java/com/example/proyecto_final/screens/mapa/MapaScreen.kt
+++ app/src/main/java/com/example/proyecto_final/screens/mapa/MapaScreen.kt
@@ -1,13 +1,10 @@
 package com.example.proyecto_final.screens.mapa
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -29,70 +26,46 @@ fun Mapas(
 
     val drawerState = rememberDrawerState(
         initialValue = DrawerValue.Closed)
-    MenuLateral(
-        navController = navController,
-        drawerState = drawerState
-    ) {
-        Contenido(
-            navController = navController,
-            drawerState = drawerState
+
+        val scope = rememberCoroutineScope()
+        val menuItems = listOf(
+            Items_menu_lateral.Item_menu_lateral4,
+            Items_menu_lateral.Item_menu_lateral1,
+            Items_menu_lateral.Item_menu_lateral2
         )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MenuLateral(
-    navController: NavController,
-    drawerState: DrawerState,
-    contenido: @Composable () -> Unit
-) {
-    val scope = rememberCoroutineScope()
-    val menuItems = listOf(
-        Items_menu_lateral.Item_menu_lateral4,
-        Items_menu_lateral.Item_menu_lateral1,
-        Items_menu_lateral.Item_menu_lateral2
-    )
-
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                menuItems.forEach { item ->
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(item.icon, contentDescription = null)
-                        },
-                        label = { Text(text = item.title) },
-                        selected = rutaActual(navController) == item.ruta,
-                        onClick = {
-                            scope.launch {
-                                drawerState.close()
-                            }
-                            navController.navigate(item.ruta) // Usa el NavHostController principal
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Botón de apertura del menú lateral
+            menuItems.forEach { item ->
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(item.icon, contentDescription = null)
+                    },
+                    label = { Text(text = item.title) },
+                    selected = rutaActual(navController) == item.ruta,
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
                         }
-                    )
-                }
+                        navController.navigate(item.ruta) // Usa el NavHostController principal
+                    }
+                )
             }
-        }
-    ) {
-        contenido()
+
+            // Mapa
+            MyGoogleMaps(
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+
     }
-}
 
-@SuppressLint("MissingPermission", "UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Contenido(
-    navController: NavController,
-    drawerState: DrawerState
-) {
-    MyGoogleMaps()
 }
-
 @Composable
-fun MyGoogleMaps(){
-    GoogleMap (
-        modifier = Modifier.fillMaxSize()
+fun MyGoogleMaps(modifier: Modifier = Modifier) {
+    GoogleMap(
+        modifier = modifier
     )
 }
+

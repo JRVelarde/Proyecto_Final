c5bef804672bfde1b591a608e8474fc296ac3629
JRVelarde <96296424+JRVelarde@users.noreply.github.com>
2024-02-13 18:52:24 +0100

    mapasv3

--- app/src/main/java/com/example/proyecto_final/screens/mapa/MapaScreen.kt
+++ app/src/main/java/com/example/proyecto_final/screens/mapa/MapaScreen.kt
@@ -1,6 +1,8 @@
 package com.example.proyecto_final.screens.mapa
 
+import android.location.Location
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,12 +11,23 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.proyecto_final.Items.Items_menu_lateral
 import com.example.proyecto_final.navigation.rutaActual
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import kotlinx.coroutines.launch
 
 
@@ -23,7 +36,7 @@ import kotlinx.coroutines.launch
 fun Mapas(
     navController: NavController
 ){
-
+    val locationService = LocationService()
     val drawerState = rememberDrawerState(
         initialValue = DrawerValue.Closed)
 
@@ -56,16 +69,48 @@ fun Mapas(
             // Mapa
             MyGoogleMaps(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxSize(),
+                locationService = locationService
             )
 
     }
 
 }
 @Composable
-fun MyGoogleMaps(modifier: Modifier = Modifier) {
+fun MyGoogleMaps(modifier: Modifier = Modifier, locationService: LocationService) {
+    val context = LocalContext.current
+
+    var userLocation by remember { mutableStateOf<Location?>(null) }
+
+    LaunchedEffect(locationService) {
+        // Llamada suspendida dentro de LaunchedEffect para evitar el error
+        userLocation = locationService.getUserLocation(context)
+    }
+
+    val cameraPositionState = rememberCameraPositionState {}
+
     GoogleMap(
-        modifier = modifier
-    )
+        modifier = modifier,
+        cameraPositionState = cameraPositionState,
+        onMapLoaded = {
+            // Puedes realizar acciones cuando el mapa se haya cargado si es necesario
+        },
+        contentPadding = PaddingValues(0.dp)
+    ) {
+        userLocation?.let {
+            // Añadir un marcador en la ubicación del usuario
+            Marker(
+                state = rememberMarkerState(position = LatLng(it.latitude, it.longitude)),
+                title = "Tu ubicación",
+                onClick = {
+                    // Acciones al hacer clic en el marcador
+                    true // Devuelve true para indicar que el clic en el marcador ha sido manejado
+                }
+            )
+        }
+    }
 }
+
+
+
 
